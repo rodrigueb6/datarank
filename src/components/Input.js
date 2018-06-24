345c8ab662ee9ddc741c6fe008442fc95258695b
Brandon Rodriguez <rodrigueb6@students.rowan.edu>
2018-06-24 17:23:11 -0400

    initial commit w/ eslint config

---
+++ src/components/Input.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export class Input extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {userInput: ''};
+    this.handleUserInput = this.handleUserInput.bind(this);
+  }
+  handleUserInput(e) {
+    this.userInput = this.setState({ userInput: e.target.value});
+                                   }
+  render() {
+    return (
+      <div>
+        <input type="text" onChange={this.handleUserInput} value={this.state.userInput}/>
+        <h1>{this.state.userInput}</h1>
+      </div>
+    );
+  }
+}

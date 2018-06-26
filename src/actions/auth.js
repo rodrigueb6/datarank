484251d917c98f775f541460117e35923769e672
Brandon Rodriguez <rodrigueb6@students.rowan.edu>
2018-06-25 22:15:27 -0400

    adding auth request (npm problems?)

---
+++ src/actions/auth.js
@@ -0,0 +1,8 @@
+import { USER_LOGGED_IN } from './components/types';
+import api from './components/api';
+
+export const userLoggedIn = user => ({
+  type: USER_LOGGED_IN,
+  user
+});
+export const login = credentials => dispatch => api.user.login(credentials).then(user => dispatch(userLoggedIn(user)));
---
+++ src/components/types.js
@@ -0,0 +1,3 @@
+export const USER_LOGGED_IN = 'USER_LOGGED_IN';
+
+export default USER_LOGGED_IN;

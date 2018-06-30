ba833e82c728e8cd4183a4677ef1696ee2aa7c1d
Brandon Rodriguez <rodrigueb6@students.rowan.edu>
2018-06-30 02:48:20 -0400

    Login and Logout functionality

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,8 +1,22 @@
-import { USER_LOGGED_IN } from '../components/types';
+import { USER_LOGGED_IN, USER_LOGGED_OUT } from '../components/types';
 import api from '../components/api';
 
 export const userLoggedIn = user => ({
   type: USER_LOGGED_IN,
   user
 });
-export const login = credentials => dispatch => api.user.login(credentials).then(user => dispatch(userLoggedIn(user)));
+
+export const userLoggedOut = () => ({
+  type: USER_LOGGED_OUT
+});
+
+export const login = credentials => dispatch =>
+  api.user.login(credentials).then(user => {
+    localStorage.datarankJWT = user.token;
+    dispatch(userLoggedIn(user));
+  });
+
+export const logout = () => dispatch => {
+  localStorage.removeItem('datarankJWT');
+  dispatch(userLoggedOut());
+};

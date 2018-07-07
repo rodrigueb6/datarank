1566fc70e830cbd68bf3bd1e4ec714ef210f5c35
Brandon Rodriguez <rodrigueb6@students.rowan.edu>
2018-07-06 23:14:49 -0400

    Reset password functionality

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import LoginForm from '../forms/LoginForm.js';
 import { login } from '../../actions/auth';
+import { Link } from 'react-router-dom';
 
 class LoginPage extends React.Component {
   submit = data => this.props.login(data).then(() => this.props.history.push('/dashboard'));
@@ -13,6 +14,8 @@ class LoginPage extends React.Component {
         <h1>Login Page</h1>
 
         <LoginForm submit={this.submit} />
+
+        <Link to="/forgot_password">Forgot Password?</Link>
       </div>
     );
   }
